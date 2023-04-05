38b36d0f68ceabc40b1588f13c27b48782889289
Yassine NAJI <67792162+N4J1@users.noreply.github.com>
2023-04-05 18:13:41 +0000

    res

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -6,36 +6,43 @@
       </a>
    </nav>
    <section class="d-flex justify-content-center align-items-center flex-grow-1">
-      <div class="container w-25">
-         <h1 class="text-center my-4 text-success fw-bold">Se Connecter</h1>
-         <hr>
-         <form action="/login" method="post">
-            @csrf
-            
-            <div class="mb-3">
-               <label for="email" class="form-label">Email</label>
-               <input type="text" class="form-control" id="email" name="email" placeholder="Email" value="{{old('email')}}" required>
-               @error('email')
-                  <p class="text-danger text-xs">{{$message}}</p>
-               @enderror
-            </div>
-               <label for="password" class="form-label">Mot de pass</label>
-            <div class="mb-3">
-               <input type="password" class="form-control" id="password" name="password" placeholder="Mot de pass" required>
-               @error('password')
-                  <p class="text-danger text-xs">{{$message}}</p>
-               @enderror
-            </div>
-            {{-- SUBMIT --}}
-            <button type="submit" class="btn btn-success form-control">Se Connecter</button>
-         </form>
-         <div class="mt-2">
-            <p>
-               Vous n'avez pas de compte ?
-              <a href="/register" class="text-primary">S'inscrire</a>
-            </p>
+  <div class="container w-md-50">
+    <h1 class="text-center my-4 text-success fw-bold fs-4 fs-md-5">Se Connecter</h1>
+    <hr>
+    <form action="/login" method="post">
+      @csrf
+      <div class="row">
+        <div class="col-md-6">
+          <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="text" class="form-control" id="email" name="email" placeholder="Email" value="{{old('email')}}" required>
+            @error('email')
+            <p class="text-danger text-xs">{{$message}}</p>
+            @enderror
           </div>
+        </div>
+        <div class="col-md-6">
+          <div class="mb-3">
+            <label for="password" class="form-label">Mot de pass</label>
+            <input type="password" class="form-control" id="password" name="password" placeholder="Mot de pass" required>
+            @error('password')
+            <p class="text-danger text-xs">{{$message}}</p>
+            @enderror
+          </div>
+        </div>
       </div>
-   </section>
+      <div class="mb-3">
+        <button type="submit" class="btn btn-success form-control col-md-12">Se Connecter</button>
+      </div>
+    </form>
+    <div class="mt-2 text-center">
+      <p>
+        Vous n'avez pas de compte ?
+        <a href="/register" class="text-primary">S'inscrire</a>
+      </p>
+    </div>
+  </div>
+</section>
+
 </body>
 </x-style>
